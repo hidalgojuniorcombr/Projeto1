92d04f438710b8a71a0de5f3505e72a10bceddd3
hidalgojuniorcombr <49489957+hidalgojuniorcombr@users.noreply.github.com>
2019-04-18 11:44:49 -0300

    Envio Materiais Iniciais Site

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>ChurchSystem</title>
+	<link rel="stylesheet" href="css/materialize.css">
+</head>
+<body>
+	<script type="text/javascript" src="js/materiliaze.js"></script>
+	<script type="text/javascript">
+		M.AutoInit();
+	</script>
+</body>
+</html>
